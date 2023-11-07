dd99b37b20b03734fc0b2527b52a3009aba2e9ed
Teks <Teks@TEKS>
2023-11-07 17:34:25 +0700

    Добавьте файлы проекта.

---
+++ laba4.1/laba4.1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+// Вариант 8
+int main() {
+    setlocale(LC_ALL, "ru");
+    int N;
+    cout << "Введите количество чисел: ";
+    cin >> N;
+    int b = 0;
+    int num;
+
+    cout << "Введите набор чисел:" << endl;
+    for (int i = 1; i <= N; i++) {
+        cin >> num;
+        if (num % 2 != 0) {
+            cout << "Порядковый номер:" << i << endl;
+            b++;
+        }
+    }
+    cout << "Количество нечетных чисел: " << b << endl;
+}
